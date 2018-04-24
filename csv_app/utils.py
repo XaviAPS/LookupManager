d956a2547c4ffae524dd1b3452a3ddfb8499ac57
XaviAPS <xavier.olivares@airplane.solutions>
2018-04-24 21:26:48 +0200

    Creation of utils.py / Code cleaning

---
+++ csv_app/utils.py
@@ -0,0 +1,81 @@
+import csv
+import sqlite3
+
+"""
+This function imports a CSV (comma delimited) into a SQLite3 DB
+Creates a table named: <document_name>
+Fills it with the CSV content
+
+args:
+    csv_root: Path of the CSV file (example: C:/Documents/example.csv)
+    db_root: Path of the database file (example: C:/Documents/my_database.db)
+    
+"""
+
+def importCSV_inDB(csv_root, db_root):
+    csv_file_name = ((csv_root.split('/'))[-1]).split('.')[0]
+    if not db_table_exists(csv_file_name):
+        # If it does not exist, we create a
+        with open(csv_root, "r") as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            headers_db = ''
+            db_connection = sqlite3.connect(db_root)
+            db_cursor = db_connection.cursor()
+            for i, csv_line in enumerate(reader):
+                if i == 0:
+                    header = csv_line[0].split(',')
+                    for head in header:
+                        if head == header[-1]:
+                            head = "`" + head + "`"
+                            head += ' text'
+                        else:
+                            head = "`" + head + "`"
+                            head += ' text, '
+                        headers_db += head
+                    db_cursor.execute('CREATE TABLE ' + csv_file_name + '\n' + '(' + headers_db + ')')
+                else:
+                    rows_db = ''
+                    row = csv_line[0].split(',')
+                    for j, element in enumerate(row):
+                        if j == len(row) - 1:
+                            element = "'" + element + "'"
+                        else:
+                            element = "'" + element + "', "
+                        rows_db += element
+                    db_cursor.execute('INSERT INTO ' + csv_file_name + ' VALUES (' + rows_db + ")")
+
+            db_connection.commit()
+            db_connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+This function returns True if database exists in our current Django project
+
+args:
+    table: String containing table name.
+    cursor: *Not necessary to introduce*
+"""
+def db_table_exists(table, cursor=None):
+    try:
+        if not cursor:
+            from django.db import connection
+            cursor = connection.cursor()
+        if not cursor:
+            raise Exception
+        table_names = connection.introspection.get_table_list(cursor)
+    except:
+        raise Exception("unable to determine if the table '%s' exists" % table)
+    else:
+        return table in table_names
--- csv_app/views.py
+++ csv_app/views.py
@@ -7,9 +7,7 @@ from django.template import loader
 import csv
 import codecs
 from csv_app.models import *
-import sqlite3
-
-# Introduces the keywords for the DB confusion
+from csv_app.utils import importCSV_inDB
 
 def index(request):
     all_documents = Document.objects.all()
@@ -17,7 +15,6 @@ def index(request):
     context = {
         'all_documents':all_documents,
     }
-    #return HttpResponse(template.render(context, request))
     return render(request, 'documents/index.html', context)
 
 
@@ -61,42 +58,8 @@ def list(request):
                     newdoc.slug = newdoc.title
                 newdoc.save()
 
-                # Adding the CSV into the DB
-                header = []
-                if not db_table_exists(newdoc.title):
-                    #If it does not exist, we create a
-                    with open("./media/"+newdoc.docfile.name, "r") as csvfile:
-                        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                        headers_db = ''
-                        db_connection = sqlite3.connect('./mydatabase')
-                        db_cursor = db_connection.cursor()
-                        for i, csv_line in enumerate(reader):
-                            if i==0:
-                                header=csv_line[0].split(',')
-                                for head in header:
-                                    if head == header[-1]:
-                                        head = "`" + head + "`"
-                                        head +=' text'
-                                    else:
-                                        head = "`" + head + "`"
-                                        head+=' text, '
-                                    headers_db+=head
-                                db_cursor.execute('CREATE TABLE ' + newdoc.title + '\n' + '(' + headers_db + ')')
-                            else:
-                                rows_db = ''
-                                row = csv_line[0].split(',')
-                                for j, element in enumerate(row):
-                                    if j == len(row)-1:
-                                        element = "'" + element + "'"
-                                    else:
-                                        element = "'" + element + "', "
-                                    print('element: '+element)
-                                    rows_db += element
-                                    print('rows_db: '+rows_db)
-                                db_cursor.execute('INSERT INTO ' + newdoc.title + ' VALUES (' + rows_db + ")")
+                importCSV_inDB('./media/' + newdoc.docfile.name, './mydatabase')
 
-                        db_connection.commit()
-                        db_connection.close()
                 # Redirect to the document list after POST
                 return HttpResponseRedirect(reverse('list'))
 
@@ -122,16 +85,4 @@ def show_view(request):
 
     return render(request, "documents/csv_read.html", locals())
 
-def db_table_exists(table, cursor=None):
-    try:
-        if not cursor:
-            from django.db import connection
-            cursor = connection.cursor()
-        if not cursor:
-            raise Exception
-        table_names = connection.introspection.get_table_list(cursor)
-    except:
-        raise Exception("unable to determine if the table '%s' exists" % table)
-    else:
-        return table in table_names
 
